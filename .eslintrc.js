cad9de06afa18f92979dd66e70587a8200c63cb0
Woojo Kim <kwoojo0127@gmail.com>
2023-10-21 02:13:33 +0900

    chore: update packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,30 @@
 module.exports = {
   env: {
-    node: true,
     browser: true,
     es2021: true,
   },
-  extends: ['standard-with-typescript', 'plugin:prettier/recommended'],
-  overrides: [],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    project: ['./tsconfig.json'],
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'import'],
   rules: {
     '@typescript-eslint/no-non-null-assertion': 'off',
     'import/order': [
